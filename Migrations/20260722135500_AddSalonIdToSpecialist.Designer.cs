12b67387cde491588932b59343849df7d3fc9f4a
Meri Chichyan <merychicyan0@gmail.com>
2026-07-22 17:57:41 +0400

    specialist register

--- Migrations/20260722135500_AddSalonIdToSpecialist.Designer.cs
+++ Migrations/20260722135500_AddSalonIdToSpecialist.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using multi_tenant_beauty_platform_back.Infrastructure.Persistence;
+using multi_tenant_beauty_platform_back.Infrastructure.Data;
 
 #nullable disable
 
